bc2df6d17a839c22e751d76cb6099d8df9faa094
csabaokros <csaba.okros@gmail.com>
2021-10-18 21:29:43 +0200

    Add cacheing to cocktail search

--- server/routes/searchByName.mjs
+++ server/routes/searchByName.mjs
@@ -2,19 +2,27 @@ import fetch from "node-fetch"
 
 import { SEARCH_TYPE, DATA_FILTERS } from "../constants.mjs"
 
+const cache = new Object(null)
+
 export const searchByName = type => async (req, res) => {
 
     const searchUrl = SEARCH_TYPE[type]
 
     if (!req.query.q) res.status(401).send("Search parameter is mandatory")
     const searchQuery = req.query.q
-
     try {
-        console.info(`Fetching ${searchUrl}${searchQuery}`);
+        let result
+        if (cache[`${searchUrl}${searchQuery}`]) {
+            console.info(`Serving ${searchUrl}${searchQuery} from cache`);
+            result = cache[`${searchUrl}${searchQuery}`]
+        } else {
+            console.info(`Fetching ${searchUrl}${searchQuery}`);
 
-        const fetchingSearchResults = fetch(searchUrl + searchQuery)
-        const resultResource = await fetchingSearchResults
-        const result = await resultResource.json()
+            const fetchingSearchResults = fetch(searchUrl + searchQuery)
+            const resultResource = await fetchingSearchResults
+            result = await resultResource.json()
+            cache[`${searchUrl}${searchQuery}`] = result
+        }
         res.end(
             JSON.stringify(
                 DATA_FILTERS[type](result)
